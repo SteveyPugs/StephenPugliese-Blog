c48af818a280b26db5a5032b1e687f2a531d9b59
Stephen Pugliese <spugliese@acupay.com>
2013-08-09 14:21:46 -0400

    Main, Single and Login Page Updates
    Added more items to main page, created single page. Created login page.

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ var Hapi = require('hapi');
 var server = Hapi.createServer('localhost', 1234, { cors: true });
 
 var home = require('./lib').Home;
+var user = require('./lib').User;
 
 
 server.route([
 	{method: 'GET', path: '/', handler: home.GetBlogEntrys},
+	{method: 'GET', path: '/post/{id}', handler: home.GetEntry},
+	{method: 'GET', path: '/login', handler: user.ShowLoginPage},
 	//All static content
 	{method: '*', path: '/{path*}', handler: {directory: {path: './static/', listing: false, redirectToSlash:true}}}
-
-	// {method: 'GET', path: '/GetForecast', handler: forecast.CurrentForecast},
-	// {method: 'GET', path: '/GetCancelDates', handler: forecast.CurrentCancelDates},
-	// {method: 'GET', path: '/GetWeather', handler: forecast.CurrentWeather},
-	// {method: 'GET', path: '/SendSMS/{phonenumber}/{status}', handler: mobile.SendText},
-	// {method: 'GET', path: '/GetAlternateSideSigns/{bor}/{street}/{num}', handler: data.getSigns},
 ]);
 
 
@@ -22,8 +19,14 @@ server.views({
         html: 'handlebars'            
     },
     path: './lib/views',
+	partialsPath: './lib/views/partials'
+});
+
+server.on('internalError', function (request, err) {
+    console.log('Error response (500) sent for request: ' + request.id + ' because: ' + err.message);
 });
 
 server.start(function(){
 	console.log('Sever Started at: ' + server.info.uri);
 });
+
