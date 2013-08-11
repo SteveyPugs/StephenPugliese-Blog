fd5fad643a4eb3307ea740da11b2c336b45584bb
Stephen Pugliese <stephen.pugliese@outlook.com>
2013-08-11 16:35:36 -0400

    Added Login Functionality

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ var home = require('./lib').Home;
 var user = require('./lib').User;
 
 
+server.auth('session', {
+    scheme: 'cookie',
+    password: 'dfad8fasjdnf89adf', //TODO: refactor this out to gitignored auth config file
+    cookie: 'blog-cookie',  //?TODO: refactor this out to gitignored auth config file
+    redirectTo: '/',
+	isSecure: false,
+	ttl: 1800000,
+	clearInvalid: true
+});
+
 server.route([
-	{method: 'GET', path: '/', handler: home.GetBlogEntrys},
-	{method: 'GET', path: '/post/{id}', handler: home.GetEntry},
-	{method: 'GET', path: '/login', handler: user.ShowLoginPage},
+	{method: 'GET', path: '/',  config: { handler: home.GetBlogEntrys, auth: { mode: 'try' }}},
+	{method: 'GET', path: '/post/{id}', config: { handler: home.GetEntry, auth: { mode: 'try' }}},
+	{method: 'GET', path: '/login', config: { handler: user.ShowLoginPage, auth: { mode: 'try' }}},
+	{method: 'POST', path: '/login/verify', config: {handler: user.VerifyLogin, auth: { mode: 'try' }}},
+	{method: 'GET', path: '/logout', config: { handler: user.Logout, auth: true}},
 	//All static content
 	{method: '*', path: '/{path*}', handler: {directory: {path: './static/', listing: false, redirectToSlash:true}}}
 ]);
@@ -21,6 +33,8 @@ server.views({
     path: './lib/views',
 	partialsPath: './lib/views/partials'
 });
+
+
 
 server.on('internalError', function (request, err) {
     console.log('Error response (500) sent for request: ' + request.id + ' because: ' + err.message);
