4a9fc5c303bc91f67a93c48f9601c2243000bbf0
Stephen Pugliese <stephen.pugliese@outlook.com>
2013-08-12 17:06:05 -0400

    Finished Sidebar Links, Menu Links

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var server = Hapi.createServer('localhost', 1234, { cors: true });
 
 var home = require('./lib').Home;
 var user = require('./lib').User;
+var admin = require('./lib').Admin;
 
 
 server.auth('session', {
@@ -18,9 +19,15 @@ server.auth('session', {
 server.route([
 	{method: 'GET', path: '/',  config: { handler: home.GetBlogEntrys, auth: { mode: 'try' }}},
 	{method: 'GET', path: '/post/{id}', config: { handler: home.GetEntry, auth: { mode: 'try' }}},
+	{method: 'GET', path: '/post-new', config: {handler: admin.PostNew, auth: { mode: 'try' }}},
+	{method: 'GET', path: '/tags-edit', config: {handler: admin.TagsEdit, auth: { mode: 'try' }}},
+	{method: 'GET', path: '/search/{term}', config: { handler: home.EntrySearch, auth: { mode: 'try' }}},
+	{method: 'GET', path: '/tags/{term}', config: { handler: home.TagSearch, auth: { mode: 'try' }}},
+	{method: 'GET', path: '/archive/{year}/{month}', config: { handler: home.ArchiveSearch, auth: { mode: 'try' }}},
 	{method: 'GET', path: '/login', config: { handler: user.ShowLoginPage, auth: { mode: 'try' }}},
 	{method: 'POST', path: '/login/verify', config: {handler: user.VerifyLogin, auth: { mode: 'try' }}},
 	{method: 'GET', path: '/logout', config: { handler: user.Logout, auth: true}},
+	
 	//All static content
 	{method: '*', path: '/{path*}', handler: {directory: {path: './static/', listing: false, redirectToSlash:true}}}
 ]);
