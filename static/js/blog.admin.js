f2fbe6bb181479a9e3cf1776fb683568aa79a05c
Stephen Pugliese <stephen.pugliese@outlook.com>
2013-08-18 21:45:48 -0400

    Finished Work on Toolbar - Cleanup

--- static/js/blog.admin.js
+++ static/js/blog.admin.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 	//https://code.google.com/p/rangyinputs/wiki/Documentation
-	
+	$('#Error-Title').hide();
+	$('#Error-Content').hide();
+
 	$("#text-bold").click(function(){
 		$("#Content").surroundSelectedText("[B]", "[/B]");
 	});
@@ -17,10 +19,98 @@ $(document).ready(function () {
 		$("#Content").replaceSelectedText("[BR]", "collapseToEnd");
 	});
 	
+	$("#text-quotes").click(function(){
+		$("#Content").surroundSelectedText("[QUOTE]", "[/QUOTE]");
+	});	
+	
+	$("#text-link-up").click(function(){
+		var linkhref = prompt("Paste Link Here");
+		if (linkhref != "")
+		{
+			$("#Content").surroundSelectedText("[A HREF='" + linkhref + "']", "[/A]");
+		}
+		else
+		{
+			alert("Link can't be blank!");
+		}	
+	});
+	
+	$("#video-youtube").click(function(){
+		var src = prompt("Enter Youtube Link");
+		if (src != "")
+		{
+			$("#Content").surroundSelectedText("[YOUTUBE HREF='" + src + "']", "[/YOUTUBE]");
+		}
+		else
+		{
+			alert("Link can't be blank!");
+		}	
+	});
+	
+	$("#image").click(function(){
+		var src = prompt("Paste Link Here");
+		if (src != "")
+		{
+			$("#Content").replaceSelectedText("[IMG SRC='" + src + "']", "collapseToEnd");
+		}
+		else
+		{
+			alert("Link can't be blank!");
+		}
+	});
 	
 	
-	// $("#text-link-up").click(function(){
-		// $("#Content").surroundSelectedText("[A]", "[/A]");
-	// });
+	//Add Edit Page Check
+	
+	$('#post-save').click(function () {
+		
+		var title = $('#Title').val(); 
+		var content = $('#Content').val();
+		
+		if (title.length == 0)
+		{
+			$('#Title').addClass("error");
+			$('#Error-Title').show();
+			$('#Error-Title').text("Length of Title needs to be > 0");
+		}
+		else
+		{
+			$('#Title').removeClass("error");
+			$('#Error-Title').hide();
+			$('#Error-Title').text("");
+		}
+		
+		
+		if (content.length == 0)
+		{
+			$('#Content').addClass("error");
+			$('#Error-Content').show();
+			$('#Error-Content').text("Length of Content needs to be > 0");
+			
+		}
+		else
+		{
+			$('#Content').removeClass("error");
+			$('#Error-Content').hide();
+			$('#Error-Content').text("");
+		}
+		
+		if (content.length != 0 && title.length != 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+		
+		
+	});
+	
+	
+	function checkEntry() {
+	
+		return false;
+	}
+	
 	
 });
--- static/js/blog.main.js
+++ static/js/blog.main.js
@@ -8,6 +8,14 @@ $(document).ready(function () {
 		text = text.replace(/\[U\]/g, "<U>");
 		text = text.replace(/\[\/U\]/g, "</U>");
 		text = text.replace(/\[BR\]/g, "<BR />");
+		text = text.replace(/\[QUOTE\]/g, "&quot;");
+		text = text.replace(/\[\/QUOTE\]/g, "&quot;");
+		text = text.replace(/\[A HREF=/g,"<A HREF=");
+		text = text.replace(/\[\/A\]/g, "</A>");
+		text = text.replace(/\[YOUTUBE HREF=/g,"<IFRAME WIDTH=100% HEIGHT=360 FRAMEBORDER=0 ALLOWFULLSCREEN SRC=");
+		text = text.replace(/\[\/YOUTUBE\]/g, "</A>");
+		text = text.replace(/\[IMG SRC=/g,"<IMG SRC=")
+		text = text.replace(/\]/g,">");
 		$(this).html(text);
 	});
 });
