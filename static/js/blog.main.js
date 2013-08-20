1c77f47b7f2de6b6f493a88f3a7e77b09a6a3dd3
Stephen Pugliese <stephen.pugliese@outlook.com>
2013-08-20 17:02:23 -0400

    Moved HTML Rendering to Server Side Code

--- static/js/blog.main.js
+++ static/js/blog.main.js
@@ -7,27 +7,6 @@ $(document).ready(function () {
 	
 	$('#Error-Name').hide();
 	$('#Error-Comment').hide();
-
-	$(".Content").each(function() {
-		var text = $(this).text();
-		text = text.replace(/\[B\]/g, "<B>");
-		text = text.replace(/\[\/B\]/g, "</B>");
-		text = text.replace(/\[I\]/g, "<I>");
-		text = text.replace(/\[\/I\]/g, "</I>");
-		text = text.replace(/\[U\]/g, "<U>");
-		text = text.replace(/\[\/U\]/g, "</U>");
-		text = text.replace(/\[BR\]/g, "<BR />");
-		text = text.replace(/\[QUOTE\]/g, "&quot;");
-		text = text.replace(/\[\/QUOTE\]/g, "&quot;");
-		text = text.replace(/\[A HREF=/g,"<A HREF=");
-		text = text.replace(/\[\/A\]/g, "</A>");
-		text = text.replace(/\[YOUTUBE HREF=/g,"<IFRAME WIDTH=100% HEIGHT=360 FRAMEBORDER=0 ALLOWFULLSCREEN SRC=");
-		text = text.replace(/\[\/YOUTUBE\]/g, "</A>");
-		text = text.replace(/\[IMG SRC=/g,"<IMG SRC=")
-		text = text.replace(/\]/g,">");
-		$(this).html(text);
-	});
-	
 	
 	//Add Comment Check
 	$('#comment-save').click(function () {
