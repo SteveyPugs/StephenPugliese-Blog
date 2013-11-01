45f4715eece1bb6ba29762758c7913b7444d913a
Stephen Pugliese <stephen.pugliese@outlook.com>
2013-10-31 23:24:30 -0400

    Finished #3 - Sequelize Implemented

--- index.js
+++ index.js
@@ -29,15 +29,14 @@ server.route([
 	{method: 'GET', path: '/tags', config: {handler: admin.Tags, auth: true}},
 	//Updates
 	{method: 'POST', path: '/post-save', config: {handler: admin.PostSave, auth: true}},
-	{method: 'POST', path: '/comment-save', config: {handler: admin.CommentSave, auth: true}},
 	{method: 'POST', path: '/tag-save', config: {handler: admin.TagsSave, auth: true}},
 	
 	
 	//Non-Admin Pags
+	{method: 'POST', path: '/comment-save', config: {handler: admin.CommentSave, auth: { mode: 'try' }}},
 	{method: 'GET', path: '/post/{id}', config: { handler: home.GetEntry, auth: { mode: 'try' }}},
 	{method: 'POST', path: '/search', config: { handler: home.EntrySearch, auth: { mode: 'try' }}},
 	{method: 'GET', path: '/tags/{term}', config: { handler: home.TagSearch, auth: { mode: 'try' }}},
-	{method: 'GET', path: '/archive/{year}/{month}', config: { handler: home.ArchiveSearch, auth: { mode: 'try' }}},
 	{method: 'GET', path: '/login', config: { handler: user.ShowLoginPage, auth: { mode: 'try' }}},
 	{method: 'GET', path: '/register', config: { handler: user.RegisterView, auth: { mode: 'try' }}},
 	{method: 'POST', path: '/register', config: { handler: user.RegisterUser}},
