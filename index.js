f6a13072675af411d806f786a870a1a33c2fa9a9
Stephen Pugliese <stephen.pugliese@outlook.com>
2014-02-24 15:10:04 -0500

    Closes #19 - Upgrade to hapi 2.x.x

--- index.js
+++ index.js
@@ -1,67 +1,170 @@
-var Hapi = require('hapi');
-var masterConfig = require('./config/config');
+var Hapi = require("hapi");
+var masterConfig = require("./config/config");
 var serverConfig = masterConfig.config;
 var server = new Hapi.Server(serverConfig.hostname, serverConfig.port, serverConfig.options);
 
-var home = require('./lib').Home;
-var user = require('./lib').User;
-var admin = require('./lib').Admin;
+var home = require("./lib").Home;
+var user = require("./lib").User;
+var admin = require("./lib").Admin;
 
+server.pack.require("hapi-auth-cookie", function (err) {
 
-server.auth('session', {
-    scheme: 'cookie',
-    password: 'dfad8fasjdnf89adf', //TODO: refactor this out to gitignored auth config file
-    cookie: 'blog-cookie',  //?TODO: refactor this out to gitignored auth config file
-    redirectTo: '/login',
-	isSecure: false,
-	ttl: 1800000,
-	clearInvalid: true
-});
-
-server.route([
-	//Home
-	{method: 'GET', path: '/',  config: { handler: home.GetBlogEntrys, auth: { mode: 'try' }}},
-	{method: 'GET', path: '/post/{id}', config: { handler: home.GetBlogEntrys, auth: { mode: 'try' }}},
-	{method: 'GET', path: '/comments/{id}', config: { handler: home.GetComments, auth: { mode: 'try' }}},
-	{method: 'GET', path: '/rss', config: { handler: home.rss, auth: { mode: 'try' }}},
-	//Pages that require Administrator
-	//Views
-	{method: 'GET', path: '/post-new', config: {handler: admin.PostNew, auth: true}},
-	{method: 'GET', path: '/post-edit/{id}', config: {handler: admin.PostEdit, auth: true}},
-	//Updates
-	{method: 'POST', path: '/post-save', config: {handler: admin.PostSave, auth: true}},
+    server.auth.strategy("session", "cookie", {
+        password: "dfad8fasjdnf89adf",
+        cookie: "EOL-cookie",
+        redirectTo: "/login",
+        isSecure: false,
+        ttl: 24 * 60 * 60 * 1000
+    });
 	
-	
-	//Non-Admin Pags
-	{method: 'POST', path: '/comment-save', config: {handler: admin.CommentSave, auth: { mode: 'try' }}},
-	
-	{method: 'GET', path: '/login', config: { handler: user.ShowLoginPage, auth: { mode: 'try' }}},
-	{method: 'GET', path: '/register', config: { handler: user.RegisterView, auth: { mode: 'try' }}},
-	{method: 'POST', path: '/register', config: { handler: user.RegisterUser}},
-	{method: 'POST', path: '/login/verify', config: {handler: user.VerifyLogin, auth: { mode: 'try' }}},
-	{method: 'GET', path: '/logout', config: { handler: user.Logout, auth: true}},
-	{method: '*', path: '/confirm/{hashkey*}', config: { handler: user.Confirm, auth: false}},
+	server.route([
+		{
+			method: "GET",
+			path: "/",
+			config: {
+				handler: home.GetBlogEntrys,
+				auth: {
+					mode: "try"
+				}
+			}
+		},
+		{
+			method: "GET",
+			path: "/post/{id}",
+			config: {
+				handler: home.GetBlogEntrys,
+				auth: {
+					mode: "try"
+				}
+			}
+		},
+		{
+			method: "GET",
+			path: "/comments/{id}",
+			config: {
+				handler: home.GetComments,
+				auth: {
+					mode: "try"
+				}
+			}
+		},
+		{
+			method: "GET",
+			path: "/rss",
+			config: {
+				handler: home.rss,
+				auth: {
+					mode: "try"
+				}
+			}
+		},
+		{
+			method: "GET",
+			path: "/post-new",
+			config: {
+				handler: admin.PostNew,
+				auth: true
+			}
+		},
+		{
+			method: "GET",
+			path: "/post-edit/{id}",
+			config: {
+				handler: admin.PostEdit,
+				auth: true
+			}
+		},
+		{
+			method: "POST",
+			path: "/post-save",
+			config: {
+				handler: admin.PostSave,
+				auth: true
+			}
+		},
+		{
+			method: "POST",
+			path: "/comment-save",
+			config: {
+				handler: admin.CommentSave,
+				auth: {
+					mode: "try"
+				}
+			}
+		},
+		{
+			method: "GET",
+			path: "/login",
+			config: {
+				handler: user.ShowLoginPage,
+				auth: {
+					mode: "try"
+				}
+			}
+		},
+		{
+			method: "GET",
+			path: "/register",
+			config: {
+				handler: user.RegisterView,
+				auth: {
+					mode: "try"
+				}
+			}
+		},
+		{
+			method: "POST",
+			path: "/register",
+			config: {
+				handler: user.RegisterUser
+			}
+		},
+		{
+			method: "POST",
+			path: "/login/verify",
+			config: {
+				handler: user.VerifyLogin,
+				auth: {
+					mode: "try" 
+				}
+			}
+		},
+		{
+			method: "GET",
+			path: "/logout",
+			config: {
+				handler: user.Logout,
+				auth: true
+			}
+		},
+		{
+			method: "*",
+			path: "/confirm/{hashkey*}",
+			config: {
+				handler: user.Confirm,
+				auth: false
+			}
+		},
+		{
+			method: "*",
+			path: "/{path*}",
+			handler: {
+				directory: {
+					path: "./static/",
+					listing: false,
+					redirectToSlash:true
+				}
+			}
+		}
+	])
 
-	
-	//All static content
-	{method: '*', path: '/{path*}', handler: {directory: {path: './static/', listing: false, redirectToSlash:true}}}
-]);
+	server.views({
+		engines: {
+			html: "handlebars"            
+		},
+		path: "./lib/views",
+		partialsPath: "./lib/views/partials"
+	});
 
-
-server.views({
-    engines: {
-        html: 'handlebars'            
-    },
-    path: './lib/views',
-	partialsPath: './lib/views/partials'
+	server.start();
 });
-
-server.on('internalError', function (request, err) {
-    console.log('Error response (500) sent for request: ' + request.id + ' because: ' + err.message);
-});
-
-server.start(function(){
-	console.log('Sever Started at: ' + server.info.uri);
-	user.setURI(server.info.uri);
-});
-
